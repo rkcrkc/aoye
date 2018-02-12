589a9fe28cabe240a955c92db7339e3c98b9bd59
RyanCrocombe <ryancrocombe@gmail.com>
2018-02-13 09:17:38 +1300

    Install letsencrypt 4

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
 
 
 	def letsencrypt
-		render text: "V-2FAd9zWf0k1N59Q0Y4YIuYKW-Q1Iqh8jmi3R1lvv8"
+		render text: "#{params[:id]}.V-2FAd9zWf0k1N59Q0Y4YIuYKW-Q1Iqh8jmi3R1lvv8"
 	end
  
 end
