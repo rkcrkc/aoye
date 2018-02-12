d84d66cba89742bb676f28720ac690bf0ac46db8
RyanCrocombe <ryancrocombe@gmail.com>
2018-02-13 08:57:43 +1300

    Install Letsencrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
+  # Let’s encrypt
+  get ‘/.well-known/acme-challenge/:id’ => ‘pages#letsencrypt’
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
